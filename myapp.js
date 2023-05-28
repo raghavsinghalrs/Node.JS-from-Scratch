a53930139c985d852690cc29c373bb82c65f5252
raghavsinghalrs <22sep2001.rs@gmail.com>
2023-05-29 00:51:49 +0530

    Resonse added

--- myapp.js
+++ myapp.js
@@ -1,3 +1,12 @@
-const a = require('fs');
+const a = require('http')
 
-a.writeFileSync('first.txt','Hello world');
+const res = a.createServer((req,res)=>{
+    console.log(req.headers);
+    res.write('<html>');
+    res.write('<head><title>First Doc</title></head>')
+    res.write('<body>Bhaijaan</body>')
+    res.write('</html')
+    res.end();
+});
+
+res.listen(3000);
