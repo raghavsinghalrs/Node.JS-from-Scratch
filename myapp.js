2f3c5ad0311ea1546647ec72c368fa006fa6570a
raghavsinghalrs <22sep2001.rs@gmail.com>
2023-05-29 16:58:27 +0530

    Url parsing

--- myapp.js
+++ myapp.js
@@ -1,12 +1,21 @@
 const a = require('http')
 
 const res = a.createServer((req,res)=>{
-    console.log(req.headers);
-    res.write('<html>');
-    res.write('<head><title>First Doc</title></head>')
-    res.write('<body>Bhaijaan</body>')
-    res.write('</html')
-    res.end();
+
+    const url = req.url;
+    if (url==='/'){
+        res.write('<html>');
+        res.write('<head><title>Enter message</title></head>')
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></body>')
+        res.write('</html')
+        res.end();
+    }
+    // console.log(req.headers);
+    // res.write('<html>');
+    // res.write('<head><title>First Doc</title></head>')
+    // res.write('<body>Bhaijaan</body>')
+    // res.write('</html')
+    // res.end();
 });
 
 res.listen(3000);
